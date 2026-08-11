aaea383831932ed19cdf1044ead52c05f77b38ae
gimmeSomePeace <toadmasterhole@gmail.com>
2026-08-11 16:21:58 +0500

    refactor: добавить роль доменным пользователям

--- domain/src/main/kotlin/me/gimmesomepeace/buywise/domain/user/User.kt
+++ domain/src/main/kotlin/me/gimmesomepeace/buywise/domain/user/User.kt
@@ -8,11 +8,14 @@ import me.gimmesomepeace.buywise.domain.shared.password.PasswordHash
  * Каждый пользователь имеет уникальный [login] и хеш пароля [passwordHash].
  * Исходный пароль пользователя не хранится.
  *
+ * [role] определяет набор административных возможностей пользователя.
+ *
  * Инварианты:
  *  - Логин должен быть уникален среди пользователей.
  */
 class User(
     val id: UserId,
     val passwordHash: PasswordHash,
-    val login: Login
+    val login: Login,
+    val role: UserRole,
 )
